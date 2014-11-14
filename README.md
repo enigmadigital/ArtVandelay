e33785b61976624e7811a02eeccf9818c4331de4
Marty Wallace <martyaced@gmail.com>
2014-11-14 11:17:04 +1100

    Definition.

---
+++ README.md
@@ -0,0 +1,1 @@
+# Art Vandelay, Importer Exporter
---
+++ artvandelay/ArtVandelayPlugin.php
@@ -0,0 +1,20 @@
+<?php namespace Craft;
+
+
+class ArtVandelayPlugin extends BasePlugin
+{
+
+	public function getName(){ return 'Art Vandelay'; }
+	public function getVersion(){ return '0.0.1'; }
+	public function getDeveloper(){ return 'XO Digital'; }
+	public function getDeveloperUrl(){ return 'http://www.xodigital.com.au'; }
+	public function hasCpSection(){ return false; }
+
+	public function registerCpRoutes()
+	{
+		return array(
+			// ??
+		);
+	}
+
+}
